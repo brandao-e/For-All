a04bf03b9cdb5ac089b68d7b2e02eb2162e69796
gabduart <gabduart68@gmail.com>
2024-08-10 14:19:18 -0300

    finalização dos arquivos: index.html, index-funcionario.html e login.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll('.nav--link');
     const themeToggleIcon = document.querySelector('.icon i');
     const body = document.body;
 
@@ -12,21 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
             themeToggleIcon.classList.add('fa-sun');
         }
     }
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            navLinks.forEach(nav => nav.classList.remove('active'));
-            this.classList.add('active');
-        });
-
-        link.addEventListener('mouseover', function() {
-            this.classList.add('hover');
-        });
-
-        link.addEventListener('mouseout', function() {
-            this.classList.remove('hover');
-        });
-    });
 
     themeToggleIcon.addEventListener('click', function() {
         body.classList.toggle('dark-mode');
@@ -41,4 +25,29 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.removeItem('theme'); // Remove o tema do localStorage (default será claro)
         }
     });
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            // Fecha todos os painéis, exceto o clicado
+            for (var j = 0; j < acc.length; j++) {
+                if (acc[j] !== this) {
+                    acc[j].classList.remove("active-acc");
+                    acc[j].nextElementSibling.style.display = "none";
+                }
+            }
+
+            // Alterna o painel clicado
+            this.classList.toggle("active-acc");
+
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
 });
